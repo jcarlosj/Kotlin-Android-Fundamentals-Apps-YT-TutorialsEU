7c90dbe9b49d214221a971bde6464119f008c981
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-05 15:57:59 -0500

    [+] Basics.tk: Definicion Variables (var) y Constantes (val)

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,5 +1,13 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    print( "It's works!" )
+    /** Declaracion de un espacio de memoria inmutable (Constante) */
+    val year = 2021
+
+    /** Declaracion de un espacio de memoria mutable (Variable) */
+    var name = "Juan"
+    println( "Hola " + name + ", estamos en el año " + year )
+
+    name = "Sofia"
+    print( "Hola ${ name }, estamos en el año ${ year }" )
 }
