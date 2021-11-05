ccf555274653e910f03f5d1907b442fda44d0523
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-05 17:01:25 -0500

    [+] Basics.tk: Contatenacion e Interpolacion de variables y cadenas

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,50 +1,11 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    // Integer numbers                                             Mínimo                          Máximo
-    var numberOne: Byte = 1               //                         -128                            +127      (8 bits)
-    var numberTwo: Short = 25_000         //                      -32 768                         +32 767      (16 bits)
-    var numberThree: Int = 8_950_000      //               -2 147 483 648                  +2 147 483 647      (32 bits)
-    var numberFour: Long = 7_834_234_000  //   -9 223 372 036 854 775 808      +9 223 372 036 854 775 807      (64 bits)
-    // NOTA: Podemos usar el guion bajo para separar por miles una cantidad y poder visualizarla mejor
+    var firstName = "Sofia"
+    var lastName = "Gutierrez"
 
-    println( "Integer numbers" )
-    println( " - Byte:  " + numberOne )
-    println( " - Short: " + numberTwo )
-    println( " - Int:   " + numberThree )
-    println( " - Long:  " + numberFour )
+    println( "Hola, " + firstName + " " + lastName + ", tu nombre tiene " + firstName.length + " letras."  )     // Concatenacion de variables y strings
+    println( "Hola, ${ firstName } ${ lastName }, tu nombre tiene ${ firstName.length } letras." )               // Interpolacion de variables y strins
+    println( "Hola, $firstName $lastName, tu nombre tiene ${ firstName.length } letras." )                       // Interpolacion de variables y strins
 
-    // Decimal numbers
-    var decimalNumberOne: Float = 5.34f                  //  (32 bits)
-    var decimalNumberTwo: Double = 5.576456842233021     //  (64 bits)
-
-    println( "Decimal numbers" )
-    println( " - Float:  " + decimalNumberOne )
-    println( " - Double: " + decimalNumberTwo )
-
-    // Booleans
-    var isSunny: Boolean = true
-
-    println( "Booleans (Boolean)" )
-    println( " - Sunny : " + isSunny )
-    println( " - Not sunny: " + ! isSunny )
-
-    // Characters
-    var digitChar: Char = '9'
-    var letterChar: Char = 'J'
-    var symbolChar: Char = '$'
-
-    println( "Characters (Char)" )
-    println( " - Digit : " + digitChar )
-    println( " - Letter: " + letterChar )
-    println( " - Symbol: " + symbolChar )
-
-    // Characters
-    var message: String = "Hello World!"
-
-    println( "String" )
-    println( " > " + message )
-    println( " - length: " + message .length )
-    println( " - first character: " + message[ 0 ] )
-    println( " - last character: " + message[ message.length - 1 ] )
 }
