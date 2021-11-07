6fb49e87c6e5d8bdf99f1cce9adb818c1268255e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-07 15:11:43 -0500

    [+] Basics.tk: Condicional when con argumentos (Tipos)

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,20 +1,16 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    println( "Android say: Dame un nombre con S, por favor " )
-    print( "Me: " )
-    var name = readLine()
-    name = name?.lowercase()
+    var something: Any = true
+    println( ": $something" )
 
     // When (Es como switch para otros lenguajes)
-    when( name ) {
-        "sofia" ->                            // Pasamos cada uno de los valore validos
-            println( "${ name } es un nombre muy bonito" )
-        "sandra", "sonia", "susana" ->                               // Pasamos un rango
-            println( "${ name } es el nombre de una chica" )
-        "sandro", "sebastian", "sergio" ->                             // Pasamos un rango
-            println( "${ name } es el nombre de un chico" )
-        else    -> println( "Hey! $name es un nombre que no conozco" )
-
+    when( something ) {
+        is Int      -> println( "$something es un 'Int'" )
+        is Double   -> println( "$something es un 'Double'" )
+        is Float    -> println( "$something es un 'Float'" )
+        is String   -> println( "$something es un 'String'" )
+        is Boolean  -> println( "$something es un 'Boolean'" )
+        else        -> println( "Hey! $something no es un tipo conocido" )
     }
 }
