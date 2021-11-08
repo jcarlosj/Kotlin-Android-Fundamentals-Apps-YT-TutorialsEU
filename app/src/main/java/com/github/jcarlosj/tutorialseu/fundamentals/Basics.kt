3bbbf49fff3553dd039f9cb441380ba15654e3af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-08 15:47:28 -0500

    [+] Basics.tk: Usando break/continue en un bucle

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,45 +1,21 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    print( "FORMA 1: \n  for ( number in 1..10 ) \n    " )
-    for ( number in 1..10 ) {
+    print( "Iterar de 1 a 20, solo imprimir hasta el 9 \n  usando break \n    " )
+    for ( number in 1..20 ) {
+        if( number / 2 == 5 ) {
+            break
+        }
         print( "$number, " )
     }
     println()
 
-    print( "FORMA 2 (until): \n  for ( i in 1 until 10 ) \n    " )
-    for ( i in 1 until 10 ) {
-        print( "$i, " )
-    }
-    println()
-
-    print( "FORMA 2 (downTo): \n  for ( i in 10 downTo 1 ) \n    " )
-    for ( i in 10 downTo 1 ) {
-        print( "$i, " )
-    }
-    println()
-
-    print( "FORMA 3 (until/step): \n  for ( i in 1 until 10 step 2 ) \n    " )
-    for ( i in 1 until 10 step 2 ) {
-        print( "$i, " )
-    }
-    println()
-
-    print( "FORMA 3 (downTo/step): \n  for ( i in 10 downTo 1 step 2 ) \n    " )
-    for ( i in 10 downTo 1 step 2 ) {
-        print( "$i, " )
-    }
-    println()
-
-    print( "FORMA 4 (until/step): \n  for ( i in 1 until( 20 ) step( 2 ) ) \n    " )
-    for ( i in 0 until( 20 ) step( 2 ) ) {
-        print( "$i, " )
-    }
-    println()
-
-    print( "FORMA 5 (downTo/step): \n  for ( i in 10.downTo( 0 ).step( 2 ) ) \n    " )
-    for ( i in 20.downTo( 0 ).step( 2 ) ) {
-        print( "$i, " )
+    print( "Iterar de 1 a 20, sin imprimir los pares \n  usando continue \n    " )
+    for ( number in 1..20 ) {
+        if( number % 2 == 0 ) {
+            continue
+        }
+        print( "$number, " )
     }
     println()
 
