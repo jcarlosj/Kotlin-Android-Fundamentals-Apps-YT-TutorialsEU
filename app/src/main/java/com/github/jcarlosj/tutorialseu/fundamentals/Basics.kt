20906b35e76b6ef85756ab0ad5cd832678bdd96e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-08 14:49:04 -0500

    [+] Basics.tk: Bucle do-while (promedio N notas)

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,26 +1,30 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    var counter: Int = 4
+    var counter: Int = 0
     var total: Float = 0.0f
     var average: Float = 0.0f
     var value: Float
 
-    println( "Promedio de 4 notas, entre 0 a 5" )
-    while( counter != 0 ) {
-        print( "Ingrese nota ${ 5 - counter }: " )
+    println( "Promedio de N notas, entre 0 a 5. Digite un numero menor de 0 para salir" )
+    repeat( 50 ){
+        print( "=" )
+    }
+    println()
+
+    do{
+        print( "Ingrese nota #${ counter + 1 }: " )
         value = readLine()!! .toFloat()
 
         if( value > 0 && value <= 5 ) {
             total += value
-            counter -= 1
+            counter ++
         }
-        else {
+        else if( value > 5 ) {
             println( "  > ERROR: La nota no esta en el rango esperado" )
         }
+    } while( value >= 0 )
 
-    }
-
-    average = ( ( total / 4 ) .toFloat() )
-    println( "El promedio de las 4 notas es: $average" )
+    average = ( ( total / counter ) .toFloat() )
+    println( "El promedio de las $counter notas ingresadas es: $average" )
 }
