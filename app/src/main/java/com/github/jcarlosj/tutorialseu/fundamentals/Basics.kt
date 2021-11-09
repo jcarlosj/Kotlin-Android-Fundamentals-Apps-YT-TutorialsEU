16fca7a9e551533310a5af64be3c7c68d112ba24
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-09 13:32:56 -0500

    [+] Basics.tk: Elvis Operator ?: asigna valor por defecto cuando es null

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -5,19 +5,18 @@ fun main() {
      *            para darle esta capacidad a las variables o constantes en Kotlin debemos hacer uso de ? al declarar su tipo (ver linea 11) */
 
     val label: String = "Android say:"
-    var message : String = "$label Hola! te preguntaré, pero no me respondas \n             ¿Cómo te llamas? "     //  Define variable de tipo nulo sin capacidad de albergar un valor nulo
+    var message : String = "$label Hola! te preguntaré, pero no me respondas \n             ¿Cómo te llamas? \nMe: "     //  Define variable de tipo nulo sin capacidad de albergar un valor nulo
 
     print( message )
     var name : String? = readLine()         //  Se declara name como una variable de tipo String Nullable anteponiendole al tipo el signo ?
 
     if( name!!.isEmpty()) {                 //  !! a diferencia del ? convierte cualquier valor en un tipo no nulo y lanza una excepción NPE (NullPointerException) si el valor es nulo
-        name = null
-        println( "$label Sin nombre el valor ahora será establecido como '${ name }'" )
-    } else {
-        println( "$label Hola ${ name?.uppercase() }! La idea era que no contestaras." )            // Con ? valida si name es null para posteriormente convertir a mayusculas
+        name = null                         //  Si el valor de name es vacio, le asignamos null
+
+        /** Elvis Operator ?: */
+        name = name ?: "Invitado"            // Si el valor de name es null va a asignarle una cadena por defecto
     }
 
-    val size = name?.let { it.length }      // Con ? valida si name no es null, posteriormente condiciona el resultado, si no es null mostrará su longitud
-    if( size != null )
-        println( "Tu nombre tiene ${ size } caracteres." )
+    println( "$label Hola $name! Bienvenido." )
+
 }
