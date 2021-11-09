8a0a69f9ebbf9958de165ecba19ea486580fdcde
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-09 15:15:08 -0500

    [+] Basics.tk: OOP, Clases/Objetos, constructores, inicializador, predeterminados, etc

--- app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
+++ app/src/main/java/com/github/jcarlosj/tutorialseu/fundamentals/Basics.kt
@@ -1,19 +1,32 @@
 package com.github.jcarlosj.tutorialseu.fundamentals
 
 fun main() {
-    /** Nullable: Es la capacidad que le damos a una variable de poder almacenar un valor nulo
-     *            para darle esta capacidad a las variables o constantes en Kotlin debemos hacer uso de ? al declarar su tipo (ver linea 11) */
+    val arepa = Arepa()
+    val sofia = Person( "Sofia", "Gutiérrez" )
+    val milo = Pet( "Milo", "Criollo" )
+    val rex = Pet()
+    val marx = Pet( petName = "Marx ")
+}
 
-    val label: String = "Android say:"
-    var message : String = "$label Hola! te preguntaré, pero no me respondas \n             ¿Cómo te llamas? \nMe: "     //  Define variable de tipo nulo sin capacidad de albergar un valor nulo
+/** OOP - Classes */
 
-    print( message )
-    var name : String? = readLine()                     //  1. Declara name como String Nullable
-    name = if( name!!.isEmpty() ) null else name        //  2. Assertion operator !! convierte cualquier variable a tipo no nulo y lanza una excepción NPE (NullPointerException) si el valor es nulo
+// Sin Constructor
+class Arepa {
+    init {
+        println( "Arepa creada!" )
+    }
+}
 
-    /** Elvis Operator ?: */
-    name = name ?: "Invitado"            // Si el valor de name es null va a asignarle una cadena por defecto
+// Con Constructor Definido (puede ser publico, private, etc)
+class Person constructor( firstName: String, lastName: String ) {
+    init {
+        println( "Persona '$firstName, $lastName' creada!" )
+    }
+}
 
-    println( "$label Hola $name! Bienvenido." )
-
+// Con Constructor No Definido y valores por defecto
+class Pet( petName: String = "Rex", petBreed: String = "German shepherd" ) {
+    init {
+        println( "Mascota '$petName, $petBreed' creada!" )
+    }
 }
